bec192963a865d11f902eecdfd9be800ae3c1655
dtaccola <danieltaccola@yahoo.com.br>
2022-11-17 19:26:44 -0300

    avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/65972640?v=4" />
+          <Avatar src="https://github.com/dtaccola.png" />
           <div className={styles.authorInfo}>
             <strong>Daniel Taccola</strong>
             <span>Web Developer</span>
@@ -20,7 +21,7 @@ export function Post() {
         <p> Fala galeraa 👋</p>
         <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
         <p><a href="">jane.design/doctorcare</a></p>
-        <p> 
+        <p>
           <a href=""> #novoprojeto</a>{' '}
           <a href=""> #nlw </a>{' '}
           <a href=""> #rocketseat </a>{' '}
@@ -29,20 +30,20 @@ export function Post() {
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-          <textarea 
-            placeholder="Deixe um comentário"
-          />
+        <textarea
+          placeholder="Deixe um comentário"
+        />
 
-          <footer>
-            <button type="submit">Publicar</button>  
-          </footer>          
-        </form>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
 
-        <div className={styles.commentList}>
-          <Comment />
-          <Comment />
-          <Comment />
-        </div>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,18 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img 
-        className={styles.cover} 
-        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" 
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt=""
       />
-      
+
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/65972640?v=4"/>
+        <Avatar src="https://github.com/maykbrito.png" />
 
         <strong>Daniel Taccola</strong>
         <span>Web Developer</span>
