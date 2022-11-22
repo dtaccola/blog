84b554a8c432dffa8527d715f55a74008650a68b
dtaccola <danieltaccola@yahoo.com.br>
2022-11-22 19:49:35 -0300

    função de like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function hadleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
@@ -28,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={hadleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
